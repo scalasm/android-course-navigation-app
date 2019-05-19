d24fd8e5d3eb64249c2fc0d534924dd20e40fdd7
Mario Scalas <mario.scalas@linksmt.it>
2019-05-20 00:09:56 +0200

    Update to lesson 3 - 21

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,10 +20,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
-
 
 class GameWonFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +33,12 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
+        binding.nextMatchButton.setOnClickListener { view ->
+            view.findNavController().navigate( GameWonFragmentDirections.actionGameWonFragmentToGameFragment2() )
+        }
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context, "Number correct ${args.numCorrect}, Number questions ${args.numQuestions}", Toast.LENGTH_LONG).show()
+
         return binding.root
     }
 }
